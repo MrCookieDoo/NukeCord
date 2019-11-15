afc5e704bffbe114c4f63e4c674849229ad601f6
toltol12 <25435680+toltol12@users.noreply.github.com>
2019-11-15 11:39:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 // Configuration
 const prefix = "$";
-const owner_ids = ["579952364828622851", "638803264351109120"];
+const owner_ids = ["579952364828622851"];
 
 function isOwner(id) {
     for(var i of owner_ids) {
